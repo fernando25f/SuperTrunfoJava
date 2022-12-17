e6cf1b63ad08ae3ca5c9587a4b8e0e67925ff299
fernando25f <fernandoasf03@gmail.com>
2022-12-17 16:53:31 -0300

    Atributos e Carta

--- src/Atributos.java
+++ src/Atributos.java
@@ -1,4 +1,41 @@
 
 public class Atributos {
-
+	private String[] atributo;
+	private double[] valor;
+	private String[] unidade;
+	private int quantidadeAtributos;
+	
+	Atributos(int quantidadeAtributos){
+		this.quantidadeAtributos=quantidadeAtributos;
+	}
+	
+	public String[] getAtributo() {
+		return this.atributo;
+	}
+	public double[] getValor() {
+		return this.valor;
+	}
+	public String[] getUnidade() {
+		return this.unidade;
+	}
+	public int getQuantidadeAtributos() {
+		return this.quantidadeAtributos;
+	}
+	
+	public void setAtributos(String[] atributo) {
+		// pegar dados
+		this.atributo = atributo;
+	}
+	public void setValores(double[] valor) {
+		// pegar dados
+		this.valor = valor;
+	}
+	public void setUnidades(String[] unidade) {
+		// pegar dados
+		this.unidade = unidade;
+	}
+	public void setQuantidadeAtributos(int quantidadeAtributos) {
+		// pegar dados
+		this.quantidadeAtributos = quantidadeAtributos;
+	}
 }
--- src/Carta.java
+++ src/Carta.java
@@ -1,4 +1,29 @@
 
-public class Carta {
+public class Carta extends Atributos{
 
+	private String codigo;
+	private String extra;
+	private String nome;
+	Carta (int quantidadeAtributos){
+		super(quantidadeAtributos);
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getExtra() {
+		return extra;
+	}
+	public void setExtra(String extra) {
+		this.extra = extra;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 }
