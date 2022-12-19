48ad2eba1beb2c823c61b28e319d52569bb21d9b
fernando25f <fernandoasf03@gmail.com>
2022-12-19 17:55:19 -0300

    Voltando ao inicio

--- src/Baralho.java
+++ src/Baralho.java
@@ -1,4 +1,4 @@
 
-public class Baralho {
+public class Baralho{
 
 }
--- src/Carta.java
+++ src/Carta.java
@@ -1,29 +1,4 @@
 
-public class Carta extends Atributos{
+public class Carta{
 
-	private String codigo;
-	private String extra;
-	private String nome;
-	Carta (int quantidadeAtributos){
-		super(quantidadeAtributos);
-	}
-	public String getCodigo() {
-		return codigo;
-	}
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
-	}
-	public String getExtra() {
-		return extra;
-	}
-	public void setExtra(String extra) {
-		this.extra = extra;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
 }
